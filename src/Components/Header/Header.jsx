77d3df3010f636745af84ad00504de9b4046db5b
saimun5656 <shaounlosted@gmail.com>
2023-04-14 12:17:24 +0600

    all components and route added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='navbar'>
+           <Link to="/">HOME</Link>
+           <Link to="/orders">Order Review</Link>
+           <Link to="/about">About</Link>
+           <Link to="/contact">Contact</Link> 
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './Components/Layout/Main'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import OrderReview from './Components/OrderReview/OrderReview'
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'about',
+        element:<About></About>
+      },
+      {
+        path:'contact',
+        element:<Contact></Contact>
+      },
+      {
+        path:'orders',
+        element:<OrderReview></OrderReview>
+      }
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
